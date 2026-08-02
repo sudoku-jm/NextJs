aeb5343cbebc115a212a493ca44563b90d7116d0
sudoku-jm <sudoku8386@gmail.com>
2026-08-02 15:23:05 +0900

    스켈레톤 UI

--- NextAppRouter/onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
+++ NextAppRouter/onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -2,6 +2,7 @@ import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { delay } from "../../../util/delay";
 import { Suspense } from "react";
+import BookListSkeleton from "../../../components/skeleton/book-list-skeleton";
 
 // export const dynamic = "error";
 
@@ -33,7 +34,7 @@ export default async function Page({
 }) {
   const { q } = await searchParams;
   return (
-    <Suspense key={q || ""} fallback={<div>Loading...</div>}>
+    <Suspense key={q || ""} fallback={<BookListSkeleton count={3} />}>
       {/* q에 값이 없으면 "" 빈값 넘기기 */}
       <SearchResult q={q || ""} />
     </Suspense>
